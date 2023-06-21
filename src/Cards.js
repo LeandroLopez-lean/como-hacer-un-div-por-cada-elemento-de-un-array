f18dac541071eb612f001360e73855093d91ea84
Leandro <leandrolopez131982@gmail.com>
2023-06-21 19:28:18 -0300

    puse el texto categoria y la X de close

--- src/Cards.js
+++ src/Cards.js
@@ -4,21 +4,30 @@ import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faExclamationCircle, } from '@fortawesome/free-solid-svg-icons';
 import { faWater } from '@fortawesome/free-solid-svg-icons';
-import { Card, Text } from '@mantine/core';
+import { Card, Text, info} from '@mantine/core';
+import { faSlidersH } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
-function Cards({ data }) {
+function Cards({ data, info }) {
   return (
     <div className=''>
-      {data.map((card, info) => (
-        <Card className='card-container'>
-          <Text>{card.texto} {card.info}</Text>
-          <div className='icon-container'>
-          <FontAwesomeIcon icon={faWater} className="water-icon" />
-          <FontAwesomeIcon icon={faDownload} className='dowloand'/>
-          <FontAwesomeIcon icon={faBars} className="menu-icon" />
-          <FontAwesomeIcon icon={faExclamationCircle} className="exclamation-icon" />
+      {data.map((card, index) => (
+        <Card key={index} className='card-container'>
+          <div>
+            <Text>{card.texto}</Text>
+            <FontAwesomeIcon className="close-icon" icon={faTimes} />
+             {/* Agregar el texto de la categoría */}
           </div>
+            <div className='icon-container'>
+              <FontAwesomeIcon icon={faWater} className="water-icon" />
+              <Text className='categori'>{info[index].item}</Text>
+              <FontAwesomeIcon icon={faDownload} className='dowloand'/>
+              <FontAwesomeIcon icon={faBars} className="menu-icon" />
+              <FontAwesomeIcon icon={faExclamationCircle} className="exclamation-icon" />
+              <FontAwesomeIcon icon={faSlidersH} className="potentiometer-icon" /> 
+            </div>
         </Card>
       ))}
     </div>
